8f6232ef9c50dd789fd9339f96c81471634c2e33
SAI Pavan <Saipavan.s.varada@gmail.com>
2026-02-04 20:01:31 +0530

    new files adding

--- SLL.c
+++ SLL.c
@@ -1,103 +1,94 @@
-/*Singly linklist(SLL) using Dynamic memory allocation and using some functions 
-create,dispaly,insert node from front and end,delete form front and end*/
-
-
-
 #include<stdio.h>
 #include<stdlib.h>
 struct node{
-    int data;
-    char usn[20];
-    char name[50];
+    char name[10];
+    int usn;
+    char program[25];
+    int sem;
+    double phone;
     struct node *next;
-    }*head;
+}*head=NULL,*temp=NULL;
 
-void create(){
-    struct node *newnode,*temp;
-    int choice;
-    printf("enter the number of students :\n");
-    scanf("%d",&choice);
-    for(int i=1;i<=choice;i++){
-    newnode=(struct node*)malloc(sizeof(struct node));
-    printf("enetr the name of student:");
-    scanf("%s",&newnode->name);
-    printf("enetr the usn of student:");
-    scanf("%s",&newnode->usn);
-    printf("enetr data:\n");
-    scanf("%d",&newnode->data);
-    newnode->next=NULL;
-    if(head==NULL){
-        head=temp=newnode;
-        }
+struct node* create(){
+    struct node *nn;
+    nn=(struct node*)malloc(sizeof(struct node));
+    printf("Enter details:\n");
+    scanf("%s%d%s%d%lf",&nn->name,&nn->usn,&nn->program,&nn->sem,&nn->phone);
+    nn->next=NULL;
+    return nn;
+}
+void insertend(){
+    struct node *nn=create();
+    if(head==NULL)
+        head=temp=nn;
+    else {
+        printf("inserted at end\n");
+        temp->next=nn;
+        temp=nn;
+    }
+}
+void inseratfront(){
+    struct node *nn =create();
+    if(head==NULL)
+        head=nn;
     else{
-        temp->next=newnode;
-        temp=newnode;
-        }
+        nn->next=head;
+        head=nn;
+    }
+}
+void deletend(){
+    struct node *prenode=head,*emp=head->next;
+    if(head==NULL){
+        printf("no elements\n");
+    }else{
+    while(emp!=NULL){
+        prenode=emp;
+        emp=emp->next;
+    }
+    prenode->next=NULL;
+}}
+void deletefront(){
+    if(head==NULL){
+        printf("no elements\n");
+    }else{
+        head=head->next;
     }
 }
 void display(){
-    struct node* temp=head;
-    int cut=0;
-    while(temp!=NULL){
-        printf("name of student:%s\n",temp->name);
-        printf("usn of student: %s\n",temp->usn);
-        printf("%d\n",temp->data);
-
-        temp=temp->next;
-
-        cut++;
+    struct node *t=head;
+    if(head==NULL){
+        printf("no records found\n");
+    }else{
+        printf("===student records===\n");
+        printf("--------------------------------------------------\n");
+        while(t!=NULL){
+            printf("name:%s|USN: %d|program: %s|SEM: %d|Phone number: %lf\n",t->name,t->usn,t->program,t->sem,t->phone);
+            t=t->next;
+        } 
+}
+}
+int main(){
+    int choice;
+    while(1){
+        printf("---records---\n");
+        printf("1.insertend\n2.insertfront\n3.deleteend\n4.deletefront\n5.display\n6.exit\n");
+        printf("enetr choice :");
+        scanf("%d",&choice);
+        switch(choice){
+            case 1: insertend();
+                    break;
+            case 2: inseratfront();
+                    break;
+            case 3: deletend();
+                    break;
+            case 4: deletefront();
+                    break;
+            case 5 :display();
+                    break;
+            case 6:exit(0);
+            default:printf("invalid choice");
+                    break;
+        }
     }
-    printf("count=%d\n",cut);
-}
-void insertfront(){
-    struct node *newnode;
-    newnode=(struct node*)malloc(sizeof(struct node));
-    printf("enetr the name of student:");
-    scanf("%s",&newnode->name);
-    printf("enetr the usn of student:");
-    scanf("%s",&newnode->usn);
-    printf("enetr data:\n");
-    scanf("%d",&newnode->data);
-    newnode->next=head;
-    head=newnode;
-}
-void insertend(){
-    struct node *newnode;
-    newnode=(struct node*)malloc(sizeof(struct node));
-    printf("enetr the name of student:");
-    scanf("%s",&newnode->name);
-    printf("enetr the usn of student:");
-    scanf("%s",&newnode->usn);
-    printf("enetr data:\n");
-    scanf("%d",&newnode->data);
-    newnode->next=NULL;
-    struct node*temp=head;
-    while(temp->next!=NULL){
-        temp=temp->next;}
-    temp->next=newnode;
-}
-void deletefront(){
-    struct node *temp=head;
-    head=head->next;
-    free(temp);
-}
-void deleteend(){
-    struct node*temp,*pre;
-    temp=pre=head;
-    while(temp->next!=NULL){
-        pre=temp;
-        temp=temp->next;}
-        free(temp);
-
-}
-void main(){
-    create();
-    display();
-    insertend();
-    insertfront();
-    display();
-    deleteend();
-    display();
-    deletefront();
-    display();
+    return 0;
 }
